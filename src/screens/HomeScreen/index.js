8c3af4fec0d2927448fc2b26b5a77102f34d868e
Cuong Gosu <cuonggosu@Cuongs-iMac.local>
2019-01-10 15:16:18 +0700

    update

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 import {
     View, Text, Image,
-    TextInput, Touchable, Keyboard,
+    TextInput, TouchableOpacity, Keyboard,
     ScrollView, StyleSheet, Dimensions,
-    Platform
+    Platform, ActivityIndicator
 } from "react-native";
 
 import Header from "../../commons/Header";
@@ -13,26 +13,60 @@ import CenterComponent from "./components/CenterComponent";
 const ICON_LEFT = require('../../resources/icons/menu.png')
 const ICON_RIGHT = require('../../resources/icons/profile.png')
 class HomeScreen extends Component {
-    render() {
-        return (
-            <ScrollView
-                //style ScrollView
-                contentContainerStyle={styles.container}
-                //Loai bo ban phim khi keo 
-                keyboardDismissMode='on-drag'
-                //Loai bo ban phim khi bam ra ngoai
-                keyboardShouldPersistTaps='handled'
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoading: false
+        }
+    }
 
-                showsVerticalScrollIndicator={false}
-            >
-                <Header
-                    leftIcon={ICON_LEFT}
-                    rightIcon={ICON_RIGHT}
-                    {...this.props}
-                />
-                <CenterComponent />
-            </ScrollView>
-        )
+    reloadView = () => {
+        this.setState({ isLoading: true })
+        setTimeout(() => {
+            this.setState({ isLoading: false })
+        }, 3000)
+    }
+
+    render() {
+        if (this.state.isLoading) {
+            return (
+                <View style={styles.viewParent}>
+                    <ActivityIndicator size='large' />
+                </View>
+            )
+        } else {
+            return (
+                <ScrollView
+                    //style ScrollView
+                    contentContainerStyle={styles.container}
+                    //Loai bo ban phim khi keo
+                    //
+                    keyboardDismissMode='on-drag'
+                    //Loai bo ban phim khi bam ra ngoai
+                    keyboardShouldPersistTaps='handled'
+
+                    showsVerticalScrollIndicator={false}
+                //Function khi keo
+                // onScroll={this.reloadView}
+                >
+                    <Header
+                        leftIcon={ICON_LEFT}
+                        rightIcon={ICON_RIGHT}
+                        {...this.props}
+                    />
+                    <CenterComponent />
+                    <TouchableOpacity
+                        style={styles.btnReload}
+                        onPress={this.reloadView}
+                    >
+                        <Text>
+                            Reload...
+                        </Text>
+                    </TouchableOpacity>
+                </ScrollView>
+            )
+        }
+
     }
 }
 
@@ -40,6 +74,19 @@ export default HomeScreen
 
 const styles = StyleSheet.create({
     container: {
-        marginTop: 20
+        marginTop: 20,
+        justifyContent: 'center',
+        // alignItems: 'center'
+    },
+    btnReload: {
+        width: '100%',
+        height: 50,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    viewParent: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
     }
 })
